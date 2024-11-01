4a31b7724baa3be1d593714e58affaa97439d7ed
Thanhf04 <thanhnt070904@gmail.com>
2024-11-01 09:28:37 +0700

    update code thuan len nhanh main

--- Assets/Script/NPC1.cs
+++ Assets/Script/NPC1.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class NPC : MonoBehaviour
+public class NPC1 : MonoBehaviour
 {
     
     [SerializeField] private GameObject Panel;
